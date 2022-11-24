9d22b260f9331aa82a098704648b9438e50b6280
DryDish <57399961+DryDish@users.noreply.github.com>
2022-11-24 19:07:51 +0100

    improve baseline

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
 
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
+
+app.get("/test", (_req, res) => {
+  res.send({ hello: "WORLD!" });
+});
 
 
 
-const PORT = process.env.APP_PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Backend App is running on port: ${PORT}`);
-    
+
+// Reject all non defined paths
+app.all('*', (_req, res) => {
+    res.status(401).send()
 })
+
+
+
+
+const PORT: number = Number(process.env.APP_PORT) || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend App is running on port: ${PORT}`);
+});
