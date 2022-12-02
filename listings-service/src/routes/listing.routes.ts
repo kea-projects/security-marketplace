92d4bdc08ca60e67a9ed006856b3f9fb50ed2047
DryDish <57399961+DryDish@users.noreply.github.com>
2022-12-02 18:07:29 +0100

    refactor existing enpoints to match diagram

--- listings-service/src/routes/listing.routes.ts
+++ listings-service/src/routes/listing.routes.ts
@@ -1,36 +1,58 @@
 import { Router } from "express";
 import { cleanListingObjFields } from "../middleware/bodyValidators";
 import { Listing } from "../models/listingModel";
-import { InternalServerError, ValidationError } from "../utils/error-messages";
+import { InternalServerError, NotFoundError, ValidationError } from "../utils/error-messages";
 import { Request, Response } from "express";
+import { validateUuidFromParams } from "../middleware/path-param-validators";
+import { validate as isValidUuid } from "uuid";
 
 const router: Router = Router();
 
-router.post("/market-entry", cleanListingObjFields, async (req: Request, res: Response) => {
-  const listing: Listing = req.body.listing
-
-  let result;
-  try {
-    listing.validate();
-    result = await listing.save();
-  } catch (error) {
-    if (error.errors) {
-      return res.status(400).send(new ValidationError(error.errors[0]));
-    } else {
-      return res.status(400).send(new ValidationError(`Foreign key '${listing.createdBy}' was not found.`));
-    }
-  }
-
-  return res.status(202).send(result);
-});
-
-router.get("/listing", async (_req: Request, res: Response) => {
+router.get("/listings", async (_req: Request, res: Response) => {
   try {
     const listingsList: Listing[] = await Listing.findAll();
     return res.status(200).send(listingsList);
   } catch {
     console.error("An error occurred retrieving the lost of market entries");
     return res.status(500).send(new InternalServerError());
+  }
+});
+
+router.get("/listings/:id", validateUuidFromParams, async (req: Request, res: Response) => {
+  const listingId = req.params.id;
+
+  try {
+    const listing: Listing | null = await Listing.findByPk(listingId);
+    if (listing) {
+      return res.status(200).send(listing);
+    } else {
+      return res.status(404).send(new NotFoundError(`Listing with id: '${listingId}' was not found in the database.`));
+    }
+  } catch {
+    console.error("An error occurred retrieving the lost of market entries");
+    return res.status(500).send(new InternalServerError());
+  }
+});
+
+router.post("/listings", cleanListingObjFields, async (req: Request, res: Response) => {
+  const listing = req.body.listing;
+  if (!isValidUuid(listing.createdBy)) {
+    return res.status(400).send(new ValidationError(`Provided UUID: '${listing.createdBy}' is not a valid UUIDv4.`));
+  }
+  // TODO: Check that the uuid matches the token.
+
+  try {
+    await listing.validate();
+    const result = await listing.save();
+    return res.status(201).send(result);
+  } catch (error) {
+    if (error.errors) {
+      console.log(error);
+      return res.status(400).send(new ValidationError(error.errors[0]));
+    } else {
+      console.error("An error occurred retrieving the listings : ", error);
+      return res.status(500).send(new InternalServerError());
+    }
   }
 });
 
--- users-service/src/routes/user.routes.ts
+++ users-service/src/routes/user.routes.ts
@@ -8,23 +8,7 @@ import { validate as isValidUuid } from "uuid";
 
 const router: Router = Router();
 
-router.post("/user", cleanUserObjFields, async (req: Request, res: Response) => {
-  const user = req.body.user;
-  if (!isValidUuid(user.userId)) {
-    return res.status(400).send(new ValidationError(`Provided UUID: '${user.userId}' is not a valid UUIDv4.`));
-  }
-
-  try {
-    await user.validate();
-    const result = await user.save();
-    return res.send(result);
-  } catch (error) {
-    console.log(error.errors);
-    return res.send(new ValidationError(error.errors[0].message));
-  }
-});
-
-router.get("/user", async (_req: Request, res: Response) => {
+router.get("/users", async (_req: Request, res: Response) => {
   try {
     const userList: User[] = await User.findAll();
     return res.status(200).send(userList);
@@ -34,21 +18,42 @@ router.get("/user", async (_req: Request, res: Response) => {
   }
 });
 
-router.get("/user/:id", validateUuidFromParams, async (req: Request, res: Response) => {
+router.get("/users/:id", validateUuidFromParams, async (req: Request, res: Response) => {
   const userId = req.params.id;
 
-  console.log(`User Id: ${userId}`);
+  // TODO: check that the id is the same as the userId in the token
 
   try {
-    const user = await User.findByPk(userId);
+    const user: User | null = await User.findByPk(userId);
     if (user) {
       return res.status(200).send(user);
     } else {
-      return res.status(404).send(new NotFoundError(`User with id: ${userId} was not found in the database.`));
+      return res.status(404).send(new NotFoundError(`User with id: '${userId}' was not found in the database.`));
     }
   } catch (error) {
     console.log("Error occurred while getting user: ", error);
     return res.status(500).send(new InternalServerError(`Unable to reach database.`));
+  }
+});
+
+router.post("/users", cleanUserObjFields, async (req: Request, res: Response) => {
+  const user = req.body.user;
+  if (!isValidUuid(user.userId)) {
+    return res.status(400).send(new ValidationError(`Provided UUID: '${user.userId}' is not a valid UUIDv4.`));
+  }
+
+  try {
+    await user.validate();
+    const result = await user.save();
+    return res.status(201).send(result);
+  } catch (error) {
+    if (error.errors) {
+      console.log(error);
+      return res.status(400).send(new ValidationError(error.errors[0]));
+    } else {
+      console.error("An error occurred retrieving the lost of market entries: ", error);
+      return res.status(500).send(new InternalServerError());
+    }
   }
 });
 
