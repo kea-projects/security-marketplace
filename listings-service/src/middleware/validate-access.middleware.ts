5e9523c7fb8b443809682a502743cf383fdf9161
Kwandes <bz.jasiek@gmail.com>
2022-12-08 16:57:08 +0100

    Add token validation logic to access middleware

--- listings-service/src/middleware/validate-access.middleware.ts
+++ listings-service/src/middleware/validate-access.middleware.ts
@@ -1,37 +1,43 @@
 import chalk from "chalk";
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { getEnvVar } from "../config/config.service";
 import { Role } from "../interfaces";
 
-const secret = getEnvVar("AUTH_SECRET", false) || "changeMe";
 /**
  * Middleware\
  * Verify if the user has the required role or admin role in their valid jwt token.\
  * It will return 401: Unauthorized if validation fails
  */
-const canAccessRoleUser = (req: Request, res: Response, next: NextFunction) => {
+const canAccessRoleUser = async (req: Request, res: Response, next: NextFunction) => {
   // validate that the request contains the jwt access token
-
   if (!req.headers || !req.headers.authorization) {
     return res.status(401).send({ message: "Unauthorized" });
   }
 
-  // Validate the token and the associated role
+  // Call Auth Service and validate the token
   try {
-    const token = req.headers.authorization.replace("Bearer ", ""); // extract the token and remove the bearer part
-    const decodedToken = jwt.verify(token, secret) as JwtPayload;
-    if (decodedToken?.role === Role.admin || decodedToken?.role === Role.user) {
-      return next();
+    const response = await fetch(`${getEnvVar("AUTH_USERS_SERVICE_URL", false)}/validate`, {
+      method: "POST",
+      headers: new Headers({
+        Authorization: req.headers.authorization,
+      }),
+    });
+    if (response.status === 200) {
+      const body = await response.json();
+      if (body.token.role === Role.admin || body.token.role === Role.user) {
+        req.body.token = body.token;
+        return next();
+      }
     }
+    console.log(new Date().toISOString() + chalk.yellow(` [WARN] Validation of an access token failed`));
+    return res.status(401).send({ message: "Unauthorized" });
   } catch (error) {
     console.log(
       new Date().toISOString() +
         chalk.redBright(` [ERROR] An error has occurred while validating an access token!`, error.stack)
     );
+    return res.status(401).send({ message: "Unauthorized" });
   }
-
-  return res.status(401).send({ message: "Unauthorized" });
 };
 
 /**
@@ -39,27 +45,35 @@ const canAccessRoleUser = (req: Request, res: Response, next: NextFunction) => {
  * Verify if the user has the required role or admin role in their valid jwt token.\
  * It will return 401: Unauthorized if validation fails
  */
-const canAccessRoleAdmin = (req: Request, res: Response, next: NextFunction) => {
+const canAccessRoleAdmin = async (req: Request, res: Response, next: NextFunction) => {
   // validate that the request contains the jwt access token
   if (!req.headers || !req.headers.authorization) {
     return res.status(401).send({ message: "Unauthorized" });
   }
 
-  // Validate the token and the associated role
+  // Call Auth Service and validate the token
   try {
-    const token = req.headers.authorization.replace("Bearer ", ""); // extract the token and remove the bearer part
-    const decodedToken = jwt.verify(token, secret) as JwtPayload;
-    if (decodedToken?.role === Role.admin) {
-      return next();
+    const response = await fetch(`${getEnvVar("AUTH_USERS_SERVICE_URL", false)}/validate`, {
+      method: "POST",
+      headers: new Headers({
+        Authorization: req.headers.authorization,
+      }),
+    });
+    if (response.status === 200) {
+      const body = await response.json();
+      if (body.token.role === Role.admin) {
+        req.body.token = body.token;
+        return next();
+      }
     }
+    return res.status(401).send({ message: "Unauthorized" });
   } catch (error) {
     console.log(
       new Date().toISOString() +
         chalk.redBright(` [ERROR] An error has occurred while validating an access token!`, error.stack)
     );
+    return res.status(401).send({ message: "Unauthorized" });
   }
-
-  return res.status(401).send({ message: "Unauthorized" });
 };
 
 export { canAccessRoleUser, canAccessRoleAdmin };
