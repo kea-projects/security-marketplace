9ac2edc5c404135a5ce03504f9b2710a1ff12009
DryDish <57399961+DryDish@users.noreply.github.com>
2022-12-09 02:39:12 +0100

    add logging to path param validators

--- users-service/src/middleware/path-param-validators.ts
+++ users-service/src/middleware/path-param-validators.ts
@@ -1,18 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { validate as isValidUuid } from "uuid";
 import { UnauthorizedError, ValidationError } from "../utils/error-messages";
+import { log } from "../utils/logger";
 
 const paramUuidValidator = (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
-
+  log.info(`Checking that the parameter id: '${id} is a valid uuid.`);
   if (!id) {
+    log.warn(`The id was invalid! rejecting...`);
     return res.status(400).send(new ValidationError("UUID was not provided."));
   }
 
   if (!isValidUuid(id)) {
+    log.warn(`The id was invalid! rejecting...`);
     return res.status(400).send(new ValidationError("The provided UUID was not valid."));
   }
-
+  log.info(`The id was valid, proceeding.`);
   return next();
 };
 
@@ -22,9 +25,10 @@ const isOwnIdValidator = (req: Request, res: Response, next: NextFunction) => {
   const userId = req.body.token.userId;
 
   if (role !== "admin" && paramId !== userId) {
+    log.warn(`The user: '${userId}' was trying to access id: '${paramId}' !`);
     return res.status(401).send(new UnauthorizedError());
   }
-
+  log.info(`The user: '${userId}' is allowed to query: '${paramId}'.`);
   return next();
 };
 
