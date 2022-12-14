3febceb2f999e5fec5639ed525ed66c6b43c1db3
CristiPV <cristi.valentin29@gmail.com>
2022-12-14 12:12:03 +0100

    add user context provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { UserContextProvider } from './context/UserContextProvider';
+import { LocalStorage } from './utils/LocalStorage';
 
 import { router } from './utils/Router';
 
 export function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <UserContextProvider userData={LocalStorage.getUser()}>
+            <RouterProvider router={router} />
+        </UserContextProvider>
+    );
 }
