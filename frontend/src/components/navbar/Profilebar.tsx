51995895da0e778c6b365763f920b420111bb637
CristiPV <cristi.valentin29@gmail.com>
2022-12-17 13:04:07 +0100

    remove useless stats

--- frontend/src/components/navbar/Profilebar.tsx
+++ frontend/src/components/navbar/Profilebar.tsx
@@ -1,20 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {
-    Text,
-    Button,
-    IconButton,
-    Spacer,
-    Badge,
-    Hide,
-    Show,
-    Popover,
-    PopoverTrigger,
-    PopoverArrow,
-    PopoverHeader,
-    PopoverBody,
-    PopoverContent,
-} from '@chakra-ui/react';
-import { HamburgerIcon } from '@chakra-ui/icons';
+import { Button, Spacer, Popover, PopoverTrigger } from '@chakra-ui/react';
 import { Navbar } from '../themed/Navbar';
 import { UserBadge } from '../UserBadge';
 import { UserApi, UserResponse } from '../../api/UserApi';
@@ -81,42 +66,6 @@ export function Profilebar({ userId }: ProfilebarProps) {
                 isLoading={isLoading}
             />
             <Spacer />
-            {/* Desktop */}
-            <Hide below="md">
-                <Text fontSize="sm" maxWidth="100px">
-                    Extra User Information <Badge colorScheme="yellow">TBD</Badge>
-                </Text>
-                <Text>
-                    Total listings <Badge colorScheme="yellow">{50}</Badge>
-                </Text>
-                <Text>
-                    Sales <Badge colorScheme="yellow">{18}</Badge>
-                </Text>
-            </Hide>
-            {/* Mobile */}
-            <Popover>
-                <Show below="md">
-                    <PopoverTrigger>
-                        <IconButton colorScheme="accent" aria-label="More Info" icon={<HamburgerIcon />} />
-                    </PopoverTrigger>
-                    <PopoverContent color="textDark">
-                        <PopoverArrow />
-
-                        <PopoverHeader>{user?.email}</PopoverHeader>
-                        <PopoverBody>
-                            <Text fontSize="sm">
-                                Extra User Information <Badge colorScheme="yellow">TBD</Badge>
-                            </Text>
-                            <Text fontSize="sm">
-                                Total listings <Badge colorScheme="yellow">{50}</Badge>
-                            </Text>
-                            <Text fontSize="sm">
-                                Sales <Badge colorScheme="yellow">{18}</Badge>
-                            </Text>
-                        </PopoverBody>
-                    </PopoverContent>
-                </Show>
-            </Popover>
 
             {((userId && isOwnProfile(userId)) || hasAdminPrivileges()) && (
                 <Popover>
