aedb3aac4b94de114d4de06bd2e143dceac93f93
Kwandes <bz.jasiek@gmail.com>
2022-12-18 17:10:28 +0100

    Add env-controlled rate limit configs

--- auth-service/src/config/rate-limiter.config.ts
+++ auth-service/src/config/rate-limiter.config.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { log } from "../utils/logger";
+import { getEnvVar } from "./config.service";
 
-const maxFailedAuthRequests = 3;
-const maxFailed404Requests = 3;
-const maxGlobalRequests = 5000;
-const rateLimitDuration = 5; // in minutes
+const maxFailedAuthRequests = Number(getEnvVar("AUTH_RATE_LIMIT_LOGIN_SIGNUP")) || 3;
+const maxFailed404Requests = Number(getEnvVar("AUTH_RATE_LIMIT_404")) || 3;
+const rateLimitDuration = Number(getEnvVar("AUTH_RATE_LIMIT_DURATION")) || 5; // in minutes
+
+let enableRateLimit = getEnvVar("GLOBAL_RATE_LIMIT_ENABLED") === "true";
+log.info(`Rate limiting enabled: ${enableRateLimit}`);
 
 export const rateLimiterAuthConfig = {
   windowMs: rateLimitDuration * 60 * 1000, // X minutes `window`
@@ -12,6 +15,7 @@ export const rateLimiterAuthConfig = {
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   skipSuccessfulRequests: true,
+  skip: (_req: Request, _res: Response) => !enableRateLimit, // Whether the rate limiting should apply at all
   message: async (req: Request, _res: Response) => {
     log.warn(`Rate limited user ${req.ip} tried to access ${req.url}`);
     return {
@@ -27,29 +31,12 @@ export const rateLimiter404Config = {
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   skipSuccessfulRequests: true,
+  skip: (_req: Request, _res: Response) => !enableRateLimit, // Whether the rate limiting should apply at all
   message: async (req: Request, _res: Response) => {
     log.warn(`Rate limited user ${req.ip} tried to access ${req.url}`);
     return {
       error: "TooManyRequests",
       message: `You can only make ${maxFailed404Requests} failed attempts every ${rateLimitDuration} minutes`,
-    };
-  },
-};
-
-/**
- * Used to limit the bandwidth of the given Ip. Window is only one minute, and all requests are included
- */
-export const rateLimiterGlobalConfig = {
-  windowMs: 1 * 60 * 1000, // X minutes `window`
-  max: maxGlobalRequests, // Limit each IP to X requests per `window`
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  skipSuccessfulRequests: false,
-  message: async (req: Request, _res: Response) => {
-    log.warn(`Rate limited user ${req.ip} tried to access ${req.url}`);
-    return {
-      error: "TooManyRequests",
-      message: `You have been making too many requests`,
     };
   },
 };
